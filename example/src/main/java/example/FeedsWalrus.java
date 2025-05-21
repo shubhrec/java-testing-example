c8f1e678585d4ddc4061f326ce564f68201d3329
Subhankar Banerjee <42092449+shubhrec@users.noreply.github.com>
2025-05-22 02:18:18 +0530

    Update FeedsWalrus.java

--- example/src/main/java/example/FeedsWalrus.java
+++ example/src/main/java/example/FeedsWalrus.java
@@ -8,7 +8,7 @@ public class FeedsWalrus {
 	OpensCan opensCan = new OpensCan();
 
 	public void feed(Walrus gary, CannedWalrusFood can) {
-		gary.addToStomach(opensCan.open(can));
+		gary.addToStomach(opensCan.open(can),can);
 	}
 
 }
